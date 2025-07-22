5399b08f1bf5b47c59999471d7c27250194bcffd
lex <sx_lex@canway.net>
2025-07-22 19:16:13 +0800

    feat: $patch

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,17 @@ import { useCounterStore } from './stores/counter'
 const counterStore = useCounterStore()
 
 console.log(counterStore.count)
+
+function fn() {
+  counterStore.$patch(state => {
+    state.count++
+  })
+  console.log(counterStore.$patch)
+}
 </script>
 
 <template>
-  <button @click="counterStore.increment">
+  <button @click="fn">
     {{ counterStore.doubleCount }} + {{ counterStore.count }}
   </button>
 </template>
--- src/pinia/store.js
+++ src/pinia/store.js
@@ -1,4 +1,4 @@
-import { computed, getCurrentInstance, inject, reactive, toRefs } from 'vue'
+import { computed, getCurrentInstance, inject, isRef, reactive, toRefs } from 'vue'
 import { PiniaSymbol } from './rootState'
 
 function createOptionStore(id, options, pinia) {
@@ -25,8 +25,39 @@ function createOptionStore(id, options, pinia) {
   createSetupStore(id, setup, pinia)
 }
 
-function createSetupStore(id, setup, pinia) {
-  const store = reactive({})
+function isObject(value) {
+  return value !== null && typeof value === 'object'
+}
+
+function createSetupStore(id, setup, pinia, isSetupStore) {
+
+  function merge(target, partialState){
+    for (const key in partialState) {
+      if(!partialState.hasOwnProperty(key)) continue
+      const targetValue =  target[key]
+      const subPatch = partialState[key]
+
+      if(isObject(subPatch) && isObject(targetValue) && !isRef(subPatch)) target[key] = merge(targetValue, subPatch) // 递归合并
+      else target[key] = subPatch
+    }
+
+    return target
+  }
+
+  function $patch(partialStateOrMutator) {
+    // 这里需要获取到原来的所有状态
+    if(typeof partialStateOrMutator !== 'function') {
+      merge(pinia.state.value[id], partialStateOrMutator)
+    }else {
+      partialStateOrMutator(pinia.state.value[id]) // 如果是函数则直接调用
+    }
+  }
+
+  const partialStore = {
+    $patch
+  }
+
+  const store = reactive(partialStore)
 
   function wrapAction(actions) {
     return function (...args) {
@@ -36,11 +67,17 @@ function createSetupStore(id, setup, pinia) {
 
   const setupStore = setup() // 拿到的setupStore 没有处理this指向
 
+  if(isSetupStore) {
+    pinia.state.value[id] = {} // 用于存放setupStore的id对应的状态
+  }
   for (const prop in setupStore) {
     const value = setupStore[prop]
     if (typeof value === 'function') {
       // 处理 actions 中的函数 this 指向问题
       setupStore[prop] = wrapAction(value)
+    } else if (isSetupStore) { // 对setupStore做一些处理操作
+      // 是用户写的componsition api
+      pinia.state.value[id][prop] = value // 将用户返回的对象里的所有属性存下来
     }
   }
 
@@ -71,7 +108,7 @@ export function defineStore(idOrOptions, setup) {
 
     if (!pinia._s.has(id)) {
 
-      if(isSetupStore) createSetupStore(id, setup, pinia)
+      if(isSetupStore) createSetupStore(id, setup, pinia, isSetupStore)
       else createOptionStore(id, options, pinia) // 创建的store只需要存在_s中即可
     }
     const store = pinia._s.get(id) // 如果已经有了store则不用创建
